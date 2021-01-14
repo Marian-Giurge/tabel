fea10e34ae7a8ff471f28d75438aacd03ff0d39a
Marian-Giurge <72253945+Marian-Giurge@users.noreply.github.com>
2021-01-14 13:03:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Referinte biogradice</title>
+</head>
+<body>
+    <div id=titlu>
+        Referințe biografice
+    </div>
+    <div>
+        <table class=table>
+            <tr>
+                <td id=col1>Capitol</td>
+                <td id=col2>Ord</td>
+                <td id=col3 colspan="2">Titlu</td>
+
+            </tr>
+            <div>
+                <tr>
+                    <td rowspan="4">HTML</td>
+
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <td>
+                        <div>Beginning HTML and CSS</div>
+                        <div id=autor>Rob Larsen, Wrox,2013</div>
+
+                    </td>
+                    <td> <button>
+                            <a href="https://www.amazon.com/Beginning-HTML-CSS-Rob-Larsen/dp/1118340183">
+                                <div class="container">
+                                    <img src="resurse/1.JPG" alt="4" class="image">
+                                    <div class="middle">
+                                        <div class="text">Pret:100 RON</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </button>
+
+                    </td>
+
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <td>
+                        <div>Head First HTML and CSS,2nd Edition</div>
+                        <div id=autor>E.Robson,E.Freeman,O'Reilly Media,2012</div>
+                    </td>
+                    <td><button>
+                            <a href="https://www.amazon.com/Head-First-HTML-CSS-Standards-Based/dp/0596159900">
+                                <div class="container">
+
+                                    <img src="resurse/2.JPG" alt="4" class="image">
+
+                                    <div class="middle">
+                                        <div class="text">Pret:150 RON</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <th>3</th>
+                    <td>
+                        <div>Build Your Own Website,2nd,Edition</div>
+                        <div id=autor>Ian Lloyd, SitePoint,2008</div>
+                    </td>
+                    <td><button>
+                            <a href="https://www.amazon.co.uk/Build-Your-Website-Right-Using/dp/0980455278">
+                                <div class="container">
+                                    <img src="resurse/3.JPG" alt="4" class="image">
+                                    <div class="middle">
+                                        <div class="text">Pret:200 RON</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </button>
+                    </td>
+                </tr>
+            </div>
+            <div>
+                <tr>
+                    <td rowspan="3">PHP</td>
+
+                </tr>
+                <tr>
+                    <th>4</th>
+                    <td>
+                        <div>Jump Start PHP</div>
+                        <div id=autor>Callum Hopkins,SitePoint,2013</div>
+                    </td>
+                    <td><button>
+                            <a href="https://www.amazon.co.uk/Jump-Start-PHP-Callum-Hopkins/dp/0987467409">
+                                <div class="container">
+                                    <img src="resurse/4.JPG" alt="4" class="image">
+                                    <div class="middle">
+                                        <div class="text">Pret:250 RON</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </button>
+                    </td>
+
+                </tr>
+                <tr>
+                    <th>5</th>
+                    <td>
+                        <div>Learning PHP, MySQL,JavaScript,CSS&HTML5,3rd Edition</div>
+                        <div id=autor>Robin Nixon,O'Reilly Media,2014</div>
+                    </td>
+                    <td><button>
+                            <a href="https://www.amazon.com/Learning-MySQL-JavaScript-HTML5-Step/dp/1491949465">
+                                <div class="container">
+                                    <img src="resurse/5.JPG" alt="4" class="image">
+                                    <div class="middle">
+                                        <div class="text">Pret:300 RON</div>
+                                    </div>
+                                </div>
+                            </a>
+                        </button>
+                    </td>
+                </tr>
+            </div>
+        </table>
+    </div>
+</body>
+</html>
